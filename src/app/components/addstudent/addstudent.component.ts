30460ab8b2663a6b09c3a65fddec2e1de09d9c84
Jashan <av.jashan333@gmail.com>
2025-04-18 14:44:48 +0530

    commit changes add students

---
+++ src/app/components/addstudent/addstudent.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addstudent',
+  imports: [],
+  templateUrl: './addstudent.component.html',
+  styleUrl: './addstudent.component.css'
+})
+export class AddstudentComponent {
+
+  Names:string[]=['Lovepreet', 'Sonu' ,'Sahil']
+
+
+}
